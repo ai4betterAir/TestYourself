f861b00bba81000fa936db7c97b5ad01121e08c7
Masrur Ahmed <67102683+ai4betterAir@users.noreply.github.com>
2026-09-17 09:47:47 +1000

    Load selective probability and statistics topic

--- js/selective-reading-quick-integrate.js
+++ js/selective-reading-quick-integrate.js
@@ -77,3 +77,4 @@ document.write('<script src="js/selective-book-topic6-fraction-sense.js"><\/scri
 document.write('<script src="js/selective-book-topic7-fraction-addsub.js"><\/script>');
 document.write('<script src="js/selective-book-topic8-fraction-multdiv.js"><\/script>');
 document.write('<script src="js/selective-book-topic9-decimals.js"><\/script>');
+document.write('<script src="js/selective-book-topic10-probability-statistics.js"><\/script>');
