b623c6b26247d32e1550a12d0b8158a3696fe9f9
Masrur Ahmed <67102683+ai4betterAir@users.noreply.github.com>
2026-09-17 09:01:57 +1000

    Load selective fraction multiplication and division topic

--- js/selective-reading-quick-integrate.js
+++ js/selective-reading-quick-integrate.js
@@ -75,3 +75,4 @@ document.write('<script src="js/selective-book-topic4-division.js"><\/script>');
 document.write('<script src="js/selective-book-topic5-factors.js"><\/script>');
 document.write('<script src="js/selective-book-topic6-fraction-sense.js"><\/script>');
 document.write('<script src="js/selective-book-topic7-fraction-addsub.js"><\/script>');
+document.write('<script src="js/selective-book-topic8-fraction-multdiv.js"><\/script>');
