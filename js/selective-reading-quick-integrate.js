9b1fe1b2f3f51cec3288b7f3f87f003d31f9abda
Masrur Ahmed <67102683+ai4betterAir@users.noreply.github.com>
2026-09-17 16:06:59 +1000

    Load Selective Topic 17 integers

--- js/selective-reading-quick-integrate.js
+++ js/selective-reading-quick-integrate.js
@@ -84,3 +84,4 @@ document.write('<script src="js/selective-book-topic13-volume-3d.js"><\/script>'
 document.write('<script src="js/selective-book-topic14-measurement-time.js"><\/script>');
 document.write('<script src="js/selective-book-topic15-coordinates-scale.js"><\/script>');
 document.write('<script src="js/selective-book-topic16-algebra-patterns.js"><\/script>');
+document.write('<script src="js/selective-book-topic17-integers.js"><\/script>');
