c8706a1a3919edb873396b29be63a9a44d4890f6
Masrur Ahmed <67102683+ai4betterAir@users.noreply.github.com>
2026-09-17 13:59:39 +1000

    Load selective coordinates scale and maps topic

--- js/selective-reading-quick-integrate.js
+++ js/selective-reading-quick-integrate.js
@@ -82,3 +82,4 @@ document.write('<script src="js/selective-book-topic11-geometry-angles.js"><\/sc
 document.write('<script src="js/selective-book-topic12-perimeter-area.js"><\/script>');
 document.write('<script src="js/selective-book-topic13-volume-3d.js"><\/script>');
 document.write('<script src="js/selective-book-topic14-measurement-time.js"><\/script>');
+document.write('<script src="js/selective-book-topic15-coordinates-scale.js"><\/script>');
